dfeb9cc3520aa39997bb0bf47f7d768d4f49c8ca
Kate <Riba0017@yandex.ru>
2020-10-14 22:37:08 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if(!matrix) return [];
+    return matrix.flatMap((arr, i) => {
+        return i % 2 !== 0 ? arr.reverse() : arr;
+    })
+};
